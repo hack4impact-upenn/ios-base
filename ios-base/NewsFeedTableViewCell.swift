e88a4d9e0b8867020624e0d6cfc27d7041510e2d
kbharathala <kbharathala@gmail.com>
2017-04-29 13:55:27 -0400

    added prof pic to table view cell

--- ios-base/NewsFeedTableViewCell.swift
+++ ios-base/NewsFeedTableViewCell.swift
@@ -12,6 +12,8 @@ class NewsFeedTableViewCell: UITableViewCell {
     @IBOutlet var usernameButton: UIButton?
     @IBOutlet var postContentLabel: UILabel?
     @IBOutlet var timestampLabel: UILabel?
+    @IBOutlet var profPicButton: UIButton?
+    
     var parent: NewsFeedViewController?
     var user: PFUser?
     
@@ -29,9 +31,33 @@ class NewsFeedTableViewCell: UITableViewCell {
         usernameButton?.setTitle(self.user!.username, for: UIControlState.normal)
         postContentLabel?.text = post.pfObject["content"] as? String
         timestampLabel?.text = post.pfObject["timeStamp"] as? String
+        
+        let userImage = self.user!["profPic"] as! PFObject
+        
+        do {
+            try userImage.fetchIfNeeded()
+        } catch let error as NSError {
+            print("Fetch failed: \(error.localizedDescription)")
+        }
+        
+        let userImageFile = userImage["imageFile"] as! PFFile
+        userImageFile.getDataInBackground {
+            (imageData, error) -> Void in
+            if error == nil {
+                if let imageData = imageData {
+                    let image = UIImage(data:imageData)
+                    self.profPicButton!.setImage(image, for: UIControlState.normal)
+                }
+            }
+        }
     }
     
     @IBAction func viewProfileButtonPressed(_ sender: UIButton) {
         self.parent?.viewProfile(user: self.user!)
     }
+    
+    @IBAction func profPicButtonPressed(_ sender: UIButton) {
+        self.parent?.viewProfile(user: self.user!)
+    }
+    
 }
